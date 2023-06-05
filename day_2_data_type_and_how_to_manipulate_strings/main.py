0c38cd1ecf0036c9552299a3742667d8a037e91b
adam_kinyua <datascience.stud.2021@gmail.com>
2023-06-04 21:20:48 -0400

    tip calculator

---
+++ day_2_data_type_and_how_to_manipulate_strings/main.py
@@ -0,0 +1,16 @@
+3 * 3 + 3 / 3 -3
+
+
+
+print("Welcome to the tip calculator")
+bill = float(input("What was the total bill? $"))
+
+tip = int(input("What percentage tip would you like to give? 10, 12, or 15?"))
+
+people = int(input("How many people are to spit the bill?"))
+
+bill_with_tip= tip / 100 * bill + bill
+bill_per_person = bill_with_tip / people
+final_amount = round(bill_per_person, 2)
+print("Each person should pay " + str(final_amount) + "dollars")
+
