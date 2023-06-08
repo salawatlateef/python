36c283240d820f918cb5a91e01252a258dde3fa8
Salawat Lateef <111083839+salawatlateef@users.noreply.github.com>
2023-06-08 12:04:56 -0400

    testing some more
    just as described above
    Some lines  were commented for  simplicity

--- day_2_data_type_and_how_to_manipulate_strings/main.py
+++ day_2_data_type_and_how_to_manipulate_strings/main.py
@@ -2,7 +2,7 @@
 
 
 
-print("Welcome to the tip calculator")
+"""print("Welcome to the tip calculator")
 bill = float(input("What was the total bill? $"))
 
 tip = int(input("What percentage tip would you like to give? 10, 12, or 15?"))
@@ -12,6 +12,9 @@ people = int(input("How many people are to spit the bill?"))
 bill_with_tip= tip / 100 * bill + bill
 bill_per_person = bill_with_tip / people
 final_amount = round(bill_per_person, 2)
-print("Each person should pay " + str(final_amount) + "dollars")
+print("Each person should pay " + str(final_amount) + "dollars")"""
 sal = "salawat lateef"
 print(sal)
+a  = 6
+b = 9
+print(f"My name is {sal}  and  I'm {a + b} years old ")
